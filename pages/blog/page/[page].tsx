c631580a40a8045fbc9e12ef2fc91348c2077b4a
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2021-03-10 22:26:50 +0000

    Start on pagination

---
+++ pages/blog/page/[page].tsx
@@ -0,0 +1,41 @@
+import { getAllPosts } from '../../../lib/api'
+import ArticleListing from '../../../components/ArticleListing'
+import { PostType, ArticleListingProps } from '../../../types'
+
+export default function PaginatedListing({ posts }): JSX.Element {
+  return  (
+    <>
+      <h1>Projects</h1>
+      <ArticleListing posts={posts} postType={PostType.Blog} />
+    </>
+  )
+}
+
+export async function getStaticProps({ params }): Promise<ArticleListingProps> {
+  const page = parseInt(params.page)
+  const perPage = 5
+  const posts = getAllPosts(PostType.Blog).slice(page-1, page + 4)
+  return {
+    props: {
+      posts,
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          page: '1',
+        }
+      },
+      {
+        params: {
+          page: '2',
+        }
+      },
+    ],
+    fallback: false,
+  }
+}
