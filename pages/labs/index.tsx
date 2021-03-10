a8e95b95454abfb93caaac9ca6f0656ec8f14a98
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2021-03-10 21:36:45 +0000

    Match existing paths

---
+++ pages/labs/index.tsx
@@ -0,0 +1,24 @@
+import { getAllPosts } from '../../lib/api'
+import { PostType, Post, ArticleListingProps } from '../../types'
+import ArticleListing from '../../components/ArticleListing'
+
+type ListingProps = {
+  posts: Array<Post>
+}
+export default function BlogListing({ posts }: ListingProps): React.ReactNode {
+  return (
+    <>
+      <h1>Labs</h1>
+      <ArticleListing posts={posts} postType={PostType.Lab} />
+    </>
+  )
+}
+
+export async function getStaticProps(): Promise<ArticleListingProps> {
+  const posts = getAllPosts(PostType.Lab)
+  return {
+    props: {
+      posts,
+    }
+  }
+}
