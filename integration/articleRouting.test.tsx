5d4918a9fb67f2faa1b73dff01374e5e2953f833
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2021-03-20 21:00:58 +0000

    Correct article routing test

--- integration/articleRouting.test.tsx
+++ integration/articleRouting.test.tsx
@@ -1,12 +1,14 @@
 import { getPage } from 'next-page-tester'
 import { mount } from 'enzyme'
 
-describe('blog listing', () => {
-  it('renders page 1 at the root', async () => {
-    const { page } = await getPage({
-      route: '/blog/page/1'
+describe('article routing', () => {
+  describe('blog listing', () => {
+    it('renders page 1 at the root', async () => {
+      const { page } = await getPage({
+        route: '/blog'
+      })
+      const wrapper = mount(page)
+      expect(wrapper.find('h1').text()).toBe('blog')
     })
-    const wrapper = mount(page)
-    expect(wrapper.find('h1').text()).toBe('blog')
   })
 })
