3fbd885ba17ebc27ea70d1c502d2f5331f7a8368
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2021-03-23 22:22:31 +0000

    Install file-loader for hard-coded assets

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 const withPreact = require('next-plugin-preact')
 
-module.exports = withPreact()
+const webpack = (config) => {
+  config.module.rules.push({
+    test: /png|jpe?g|gif|woff|woff2|ico/,
+    use: [
+      {
+        loader: 'file-loader',
+        options: {
+          name: '[name]-[contenthash].[ext]',
+          outputPath: './static/assets',
+          publicPath: './_next/static/assets'
+        }
+      }
+    ]
+  })
+  return config
+}
+
+module.exports = withPreact({ webpack })
