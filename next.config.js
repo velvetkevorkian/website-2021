2ded4e122ce5ade79f50e2328fde106ab6f088a0
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2021-04-02 17:09:04 +0100

    Use Webpack 5 - fast refresh with Preact seems fine now - importing assets now supported without extra config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,5 @@
 const withPreact = require('next-plugin-preact')
 
-const webpack = (config) => {
-  config.module.rules.push({
-    test: /png|jpe?g|gif|woff|woff2|ico/,
-    use: [
-      {
-        loader: 'file-loader',
-        options: {
-          name: '[name]-[contenthash].[ext]',
-          outputPath: './static/assets',
-          publicPath: './_next/static/assets'
-        }
-      }
-    ]
-  })
-  return config
-}
-
-module.exports = withPreact({ webpack })
+module.exports = withPreact({ future: {
+  webpack5: true,
+} })
