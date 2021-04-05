54e526a949868b2e1f5f57176c6d09f70b509020
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2021-04-05 22:49:39 +0100

    Start setting up default <head>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 import Header from 'components/Header'
 import Nav from 'components/Nav'
@@ -11,6 +12,10 @@ if(process.env.NODE_ENV === 'development') {
 export default function App({ Component, pageProps }: AppProps): React.ReactNode {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Kyle Macquarrie</title>
+      </Head>
       <Header />
       <Nav />
       <main id="content">
