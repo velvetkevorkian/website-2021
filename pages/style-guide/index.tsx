e82cfa7a24e181c5821ac8b3f23a7eda8db29d8c
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2021-04-13 16:40:24 +0100

    Use Center component in style guide

--- pages/style-guide/index.tsx
+++ pages/style-guide/index.tsx
@@ -1,19 +1,22 @@
 import { fakeMarkdown } from 'lib/api'
 import SkipLink from 'components/SkipLink'
 import Pagination from 'components/Pagination'
+import Center from 'components/Center'
 import { PostType } from 'types'
 import Head from 'next/head'
 import pageTitle from 'lib/pageTitle'
 
-export default function StyleGuide({ htmlContent }: { htmlContent: string }): JSX.Element {
+export default function StyleGuide({ htmlContent }: { htmlContent: string }) {
   return (
     <>
       <Head>
         <title>{pageTitle({ pageTitle: 'Style Guide'})}</title>
       </Head>
       <details>
-        <summary>Text styling</summary>
-        <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+        <summary>Text styling in a {`<Center />`}</summary>
+        <Center>
+          <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+        </Center>
       </details>
 
       <details>
