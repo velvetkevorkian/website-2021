8663a46b6ea0f96804213f0d71e5917b94547a1f
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2021-04-16 17:37:22 +0100

    Add next-images plugin

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 const withPreact = require('next-plugin-preact')
+const withImages = require('next-images')
 
-module.exports = withPreact({ future: {
-  webpack5: true,
-} })
+module.exports = withPreact(withImages({
+  future: {
+    webpack5: true,
+  },
+  esModule: true,
+  inlineImageLimit: 0,
+}))
