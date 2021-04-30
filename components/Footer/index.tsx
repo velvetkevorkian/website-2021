ac35dd0bbbd371268255bb17a55d9076c56a6016
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2021-04-30 23:29:42 +0100

    More footer links

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,12 +1,19 @@
 import Link from 'next/link'
 import twitter from 'assets/images/twitter.svg'
+import email from 'assets/images/email.svg'
+import github from 'assets/images/github.svg'
+import linkedin from 'assets/images/linkedin.svg'
 import styles from './styles.module.css'
 
 export default function Footer() {
   const links = [
     { title: 'Follow me on Twitter', href: 'https://twitter.com/k_macquarrie', icon: twitter },
-    { title: 'Style guide', href: '/style-guide' },
-  ]
+    { title: 'Email me', href: 'mailto:kyle@kylemacquarrie.co.uk', icon: email },
+    { title: 'Follow me on Github', href: 'https://github.com/velvetkevorkian', icon: github },
+    { title: 'Follow me on LinkedIn', href: 'https://www.linkedin.com/in/kmacquarrie/', icon: linkedin },
+    process.env.NODE_ENV !== 'production' && { title: 'Style guide', href: '/style-guide' },
+  ].filter(Boolean)
+
   return (
     <footer>
       <nav>
@@ -20,7 +27,7 @@ export default function Footer() {
                       <use href={`${l.icon}#icon`}/>
                     </svg>
                   )}
-                  {l.title}
+                  <span className={l.icon ? 'visually-hidden' : ''}>{l.title}</span>
                 </a>
               </Link>
             </li>
