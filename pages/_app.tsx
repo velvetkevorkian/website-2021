1915ed9dd8e3de44b47be7cc40dc48b3866e44e0
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2021-11-11 22:24:55 +0000

    initial barcode styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,12 +7,14 @@ import pageTitle from 'lib/pageTitle'
 import 'assets/css/fonts.css'
 import 'assets/css/variables.css'
 import 'assets/css/app.css'
+import favicon from 'assets/images/favicon.svg'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href={favicon} />
         <title>{pageTitle()}</title>
       </Head>
 
